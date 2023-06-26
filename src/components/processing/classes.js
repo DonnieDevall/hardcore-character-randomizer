59d39e206a062791b4c0e14ca4bf6e8154b8214a
Donovan DeVall <donovandevall@gmail.com>
2023-06-26 08:59:29 -0400

    first commit

---
+++ src/components/processing/classes.js
@@ -0,0 +1,28 @@
+const classes = {
+    hunter: {
+        img: '../assets/images/classes/Ui-charactercreate-classes_hunter.png'
+    },
+    mage: {
+        img: '../assets/images/classes/Ui-charactercreate-classes_mage.png'
+    },
+    paladin: {
+        img: '../assets/images/classes/Ui-charactercreate-classes_paladin.png'
+    },
+    priest: {
+        img: '../assets/images/classes/Ui-charactercreate-classes_priest.png'
+    },
+    rogue: {
+        img: '../assets/images/classes/Ui-charactercreate-classes_rogue.png'
+    },
+    shaman: {
+        img: '../assets/images/classes/Ui-charactercreate-classes_shaman.png'
+    },
+    warlock: {
+        img: '../assets/images/classes/Ui-charactercreate-classes_warlock.png'
+    },
+    warrior: {
+        img: '../assets/images/classes/Ui-charactercreate-classes_warrior.png'
+    },
+}
+
+export default classes;
