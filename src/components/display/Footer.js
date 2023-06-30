5808da2912c588f1c166d3d345a983210f645646
Donovan DeVall <donovandevall@gmail.com>
2023-06-29 20:11:16 -0400

    fixed footer link css

--- src/components/display/Footer.js
+++ src/components/display/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div className='footer'>
-            <p>Hardcore Character Randomizer created by <span className='footer-name'>Donovan</span>. Race and Class icons property of Blizzard.</p>
+            <p>Hardcore Character Randomizer created by <a className='footer-name' href="https://github.com/DonnieDevall">DonnieDev</a>. Race and Class icons property of Blizzard.</p>
         </div>
     )
 }
